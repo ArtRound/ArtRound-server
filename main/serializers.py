7d6f1dee77ad51de6490b3473a5e82468169e006
Jsgithubchannel <jsschannel99@gmail.com>
2021-09-08 20:48:08 +0900

    Feat: add Question in serializers, views

--- main/serializers.py
+++ main/serializers.py
@@ -6,3 +6,9 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ('id', 'title', 'content', 'updated_at')
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ('id', 'useremail', 'type', 'title', 'content', 'updated_at')
--- main/views.py
+++ main/views.py
@@ -3,8 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
 
-from .serializers import ReviewSerializer
-from .models import Review
+from .serializers import ReviewSerializer, QuestionSerializer
+from .models import Review, Question
 
 
 class ReviewList(APIView):
@@ -53,4 +53,49 @@ class ReviewDetail(APIView):
     def delete(self, request, pk, format=None):
         review = self.get_object(pk)
         review.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class QuestionList(APIView):
+    # 블로그 목록 보여줄 때
+    def get(self, request):
+        questions = Question.objects.all()
+        # 여러개 객체 serialize하려면 many=True
+        # TO JSON Format
+        serializer = QuestionSerializer(questions, many=True)
+        return Response(serializer.data)
+
+    # 새 글 작성시
+    def post(self, request):
+        serializer = QuestionSerializer(
+            data=request.data)  # request.data는 사용자 입력 데이터
+        if serializer.is_valid():  # 유효성 검사
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class QuestionDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Question.objects.get(pk=pk)
+        except Question.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        question = self.get_object(pk)
+        serializer = QuestionSerializer(question)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        question = self.get_object(pk)
+        serializer = QuestionSerializer(question, data=request.data)
+        if serializer.is_valid():  # 유효성 검사
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        question = self.get_object(pk)
+        question.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
