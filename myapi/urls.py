38f7d50856d8e49b3508f798a9e7af7631acc25c
Jsgithubchannel <jsschannel99@gmail.com>
2021-12-01 21:00:23 +0900

    edit main url

--- myapi/urls.py
+++ myapi/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('main.urls')),
+    path('main/', include('main.urls')),
 ]
