c6f2611b467ad29c4dd030686a8c0e8663d553ba
Jsgithubchannel <jsschannel99@gmail.com>
2021-12-05 16:50:36 +0900

    add Answer views, urls

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import ReviewList, ReviewDetail
 from .views import QuestionList, QuestionDetail
+from .views import AnswerList, AnswerDetail
 from .views import NoticeList, NoticeDetail
 from .views import FavoritesList, FavoritesDetail
 
@@ -12,6 +13,8 @@ urlpatterns = [
     path('review/<int:pk>/', ReviewDetail.as_view()),
     path('question/', QuestionList.as_view()),
     path('question/<int:pk>/', QuestionDetail.as_view()),
+    path('answer/', AnswerList.as_view()),
+    path('answer/<int:pk>/', AnswerDetail.as_view()),
     path('notice/', NoticeList.as_view()),
     path('notice/<int:pk>/', NoticeDetail.as_view()),
     path('favorites/', FavoritesList.as_view()),
--- main/views.py
+++ main/views.py
@@ -3,8 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
 
-from .serializers import ReviewSerializer, QuestionSerializer, NoticeSerializer, FavoritesSerializer
-from .models import Review, Question, Notice, Favorites
+from .serializers import ReviewSerializer, QuestionSerializer, AnswerSerializer, NoticeSerializer, FavoritesSerializer
+from .models import Review, Question, Answer, Notice, Favorites
 
 
 class ReviewList(APIView):
@@ -97,6 +97,48 @@ class QuestionDetail(APIView):
     def delete(self, request, pk, format=None):
         question = self.get_object(pk)
         question.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+#--------------------------------------------------------------------------------
+class AnswerList(APIView):
+    def get(self, request):
+        answers = Answer.objects.all()
+        serializer = AnswerSerializer(answers, many=True)
+        return Response(serializer.data)
+
+    # 새 글 작성시
+    def post(self, request):
+        serializer = AnswerSerializer(
+            data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AnswerDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Answer.objects.get(pk=pk)
+        except Answer.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        answer = self.get_object(pk)
+        serializer = AnswerSerializer(answer)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        answer = self.get_object(pk)
+        serializer = AnswerSerializer(answer, data=request.data)
+        if serializer.is_valid():  # 유효성 검사
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        answer = self.get_object(pk)
+        answer.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 #--------------------------------------------------------------------------------
