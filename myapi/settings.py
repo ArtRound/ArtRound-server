4b093d061fc072ab918f62aac119bd1ab8348ec4
Jsgithubchannel <jsschannel99@gmail.com>
2021-12-26 14:40:05 +0900

    setup kakao login

--- myapi/settings.py
+++ myapi/settings.py
@@ -85,7 +85,28 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000')
 CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
 
 ROOT_URLCONF = 'myapi.urls'
 
