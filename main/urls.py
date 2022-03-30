d31ffcab29933baf38248aa58905cc2b6ce12272
jeryjayjae <hjk0639@pukyong.ac.kr>
2022-03-31 03:10:06 +0900

    Comment : urls

--- main/urls.py
+++ main/urls.py
@@ -24,20 +24,18 @@ urlpatterns = [
     path('notice/', NoticeList.as_view()),
     path('notice/<int:pk>/', NoticeDetail.as_view()),
     
-    path('favorites/', FavoritesList.as_view()),
-    path('favorites/<int:pk>', FavoritesList.as_view()),
+    path('favorites/', FavoritesList.as_view()), # 즐겨찾기 목록
+    path('favorites/<int:pk>', FavoritesList.as_view()), # 즐겨찾기 등록
     path('favorites/<int:pk>', FavoritesDetail.as_view()),
     
     path('art_info/', ArtInfoList.as_view()), # 전시회들
     path('art_info/<int:pk>', ArtInfoDetail.as_view()), # 전시회 정보
-    path('art_info/<int:pk>/favorites', FavoritesList.as_view()), # 즐겨찾기 등록
-    path('art_info/<int:pk>/visited', VisitedList.as_view()), # 방문했다요 등록
     path('art_info/<int:pk>/review/', ReviewList.as_view()), # 전시회 리뷰
     path('art_info/<int:pk>/review/<int:pk2>/', ReviewDetail.as_view()), # 전시회 리뷰 디테일
     
     
-    path('visited/', VisitedList.as_view()),
-    path('visited/<int:pk>', VisitedList.as_view()),
+    path('visited/', VisitedList.as_view()), # 방문 목록
+    path('visited/<int:pk>', VisitedList.as_view()), # 방문 등록
     path('visited/<int:pk>', VisitedDetail.as_view()),
 
     path('login/kakao/', views.kakao_login, name='kakao_login'),
